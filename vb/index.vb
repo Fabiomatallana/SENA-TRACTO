9464653a57c2d5bc716b7ef86a86ad8ad56f0d5c
unknown <fabiomatallana.82@gmail.com>
2024-04-17 18:52:07 -0500

    Primera versión

---
+++ vb/index.vb
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>CARDIO</title>
+  
+       </head>
+    <body>
+
+        
+
+            <img clase="bg" src="images/br.svg" alt="">
+        </header>
+       <h1> TUTORIAL COMO HACER CARDIO</h1>
+       <p>es un ejercicio fisico, muscular y aerobico y este regularmente aporta muchos beneficios a la salud. Aumenta la energia y disminuye el porcentaje muscular.
+     </p>
+     <img src="pho.webp" alt=""width="15%">
+     <h2>1.CALENTAMIENTO</h2>
+     <P>Como realizar el calentamiento haz ejercicios de intensidad baja durante un tiempo de 5 a 10 minutos para que se estimule el sistema cardio y aumentar el flujo sanguineo.ejemplo seria si es un gym utilizar la caminadora o caminar a un paso constante.</P>
+     <img src="pho2.jpeg" alt=""width="15%">
+     <h3>2. ACONDICIONAMIENTO</h3>
+     <P>Es esencial para la preparacion del cuerpo para empezar con la actividad fisica en este ya se puede incluir trotar, correr andar en bisicleta o nadar tiene un tiempo estimado de 20 minutos.</P>
+     <img src="bici.jpeg" alt=""width="15%">
+     <h4>3. ENFRIAMIENTO</h4>
+     <P>Por ultimo y no menos importante despues de realizar cualquier actividad fisica es importante realizar un estiramineto de los musculos trabajados para evitar calambres y dolores muculares. siempre que se realiza una actividad fisca es importante el consumo de agua para la hidratación del cuerpo</P>
+     <img src="agua.webp" alt=""width="10%">
+    
+    </body>
+</html>
---
+++ vb/style.vb
@@ -0,0 +1,25 @@
+*{
+    background-color: rgb(114, 145, 83);
+}
+p{font-size: 25px;}
+
+table, th, tr,td{
+    border: 2px solid;
+}
+
+.arriba{
+    color: rgb(162, 0, 255);
+}
+
+.medio{
+    color: rgb(15, 105, 93);
+}
+
+p{
+    color: rgb(87, 9, 9);
+}
+#en1
+ {
+    front-weight: 400;
+ }
+ 
